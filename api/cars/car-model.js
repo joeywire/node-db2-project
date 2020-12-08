e567436652f82c120a9c912213f99437099ed913
joeywire <61367542+joeywire@users.noreply.github.com>
2020-12-08 17:10:44 -0600

    Looking pretty crudy

--- api/cars/car-model.js
+++ api/cars/car-model.js
@@ -1,7 +1,20 @@
+
 const db = require('../../data/dbConfig');
 
 module.exports = {
   getAll() {
     return db('car-dealer');
+  }, 
+  getById(id) {
+    return db('car-dealer').where('id', id).first();
+  }, 
+  create(car) {
+    return db('car-dealer').insert(car);
+  },
+  update(id, changes) {
+    return db('car-dealer').where('id', id).update(changes);
+  },
+  delete(id) {
+    return db('car-dealer').where('id', id).del();
   }
 };
--- api/cars/car-router.js
+++ api/cars/car-router.js
@@ -12,4 +12,47 @@ router.get('/', async (req, res) => {
   }
 });
 
+router.get('/:id', async (req, res) => {
+  const { id } = req.params; 
+  try {
+    const car = await Car.getById(id);
+    res.status(200).json(car);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+router.post('/', async (req, res) => {
+  const car = req.body;
+  try {
+    //This is 100x better than chained promises 
+    const [ newCarId ] = await Car.create(car);
+    const newCar = await Car.getById(newCarId);
+    res.status(200).json(newCar);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+router.put('/:id', async (req, res) => {
+  const { id } = req.params;
+  const changes = req.body;
+  try {
+    const updatedCar = await Car.update(id, changes);
+    res.status(200).json(updatedCar);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+router.delete('/:id', async (req, res) => {
+  const { id } = req.params;
+  try {
+    const deleted = await Car.delete(id);
+    res.status(200).json({ message: `Deleted car with id: ${id}`});
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  } 
+});
+
 module.exports = router;
